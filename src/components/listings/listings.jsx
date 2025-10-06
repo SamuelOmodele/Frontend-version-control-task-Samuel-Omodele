9e35207211bea05e4186b6004ddb7cb74ea333a5
SamuelOmodele <samuelomodele19@gmail.com>
2025-10-06 10:43:27 +0100

    chore: removed listing card button function

--- src/components/listings/listings.jsx
+++ src/components/listings/listings.jsx
@@ -6,7 +6,6 @@ import house3 from '../../assets/house3.jpg'
 import house4 from '../../assets/house4.jpg'
 import house5 from '../../assets/house5.jpg'
 import house6 from '../../assets/house6.webp'
-import { useNavigate } from "react-router-dom";
 
 const Listings = () => {
   const listings = [
@@ -55,8 +54,6 @@ const Listings = () => {
   ];
 
 
-  const navigate = useNavigate();
-
   return (
     <section className={styles.featured} id="listings">
       <h2>Our Listings</h2>
@@ -67,7 +64,7 @@ const Listings = () => {
             <div className={styles.cardBody}>
               <h3><span>{listing.title}</span> - {listing.price}</h3>
               <p>{listing.details}</p>
-              <button onClick={() => navigate(`/property/${listing.id}`)}>View Details</button>
+              <button>View Details</button>
             </div>
           </div>
         ))}
