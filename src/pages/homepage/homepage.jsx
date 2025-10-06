7666817c9ac0a85aacd01515889816925867ecca
SamuelOmodele <samuelomodele19@gmail.com>
2025-10-06 09:37:32 +0100

    chore: imported navbar component to homepage component

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from '../../components/navbar/navbar'
 
 const HomePage = () => {
     return (
         <div style={{ paddingTop: '80px' }}>
+            <Navbar />
         </div>
     )
 }
